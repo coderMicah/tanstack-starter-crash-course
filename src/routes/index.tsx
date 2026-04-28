4ba7afde3f67cdbe26f15f99690bbaeb9cfad831
coderMicah <ismiki977@gmail.com>
2026-04-28 19:26:56 +0300

    implement homepage and skillcard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,58 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from "@tanstack/react-router";
+import { TerminalIcon } from "lucide-react";
+import SkillCard from "#/components/skill-card";
+import { dummySkills, skills } from "#/lib/dummy-skills";
 
-export const Route = createFileRoute('/')({ component: Home })
+export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
-  return (
-    <div className="p-8">
-      <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-      <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started.
-      </p>
-    </div>
-  )
+	return (
+		<div id="home">
+			<section className="hero">
+				<div className="copy">
+					<h1>
+						The Registry for <br />{" "}
+						<span className="text-gradient">Agentic Intelligence</span>
+					</h1>
+					<p>
+						Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea
+						pariatur nemo est labore esse placeat tempore nihil, nesciunt
+						repellat doloribus.
+					</p>
+				</div>
+
+				<div className="actions">
+					<Link to="/skills" className="btn-primary">
+						<TerminalIcon size={18} />
+						<span>Browse Registry</span>
+					</Link>
+					<Link to="/skills/new" className="btn-secondary">
+						<TerminalIcon size={18} />
+						<span>Publish Skills</span>
+					</Link>
+				</div>
+
+				<div className="latest">
+					<div className="space-y-2">
+						<h2>
+							Recently Created <span className="text-gradient">Skills</span>
+						</h2>
+						<p>Latest skills loaded from database in descending order </p>
+					</div>
+				</div>
+
+				<div>
+					{dummySkills.length > 0 ? (
+						<div className="skills-grid">
+							{dummySkills.map((skill) => (
+								<SkillCard key={skill.id} {...skill} />
+							))}
+						</div>
+					) : (
+						<p>No skills have been created yet.</p>
+					)}
+				</div>
+			</section>
+		</div>
+	);
 }
