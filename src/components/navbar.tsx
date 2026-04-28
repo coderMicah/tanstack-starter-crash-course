41d19b5ecb86a6eb23a1270d30b043d5e9afa28f
coderMicah <ismiki977@gmail.com>
2026-04-28 20:11:49 +0300

    feat:add auth

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,4 @@
+import { Show, UserButton } from "@clerk/tanstack-react-start";
 import { Link } from "@tanstack/react-router";
 import { LogIn } from "lucide-react";
 
@@ -15,9 +16,16 @@ const Navbar = () => {
 			</div>
 
 			<div className="actions">
-				<Link to="/sign-in/$" className="btn-primary">
-					<LogIn size={16} /> Sign In
-				</Link>
+				<Show when="signed-in">
+					<UserButton />
+				</Show>
+				<Show when="signed-out">
+					<Link to="/sign-in/$" className="btn-primary">
+						<LogIn size={16} /> Sign In
+					</Link>
+					{/* <SignInButton mode="modal" /> */}
+					{/* <SignUpButton /> */}
+				</Show>
 			</div>
 		</nav>
 	);
